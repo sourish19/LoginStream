048354da1c1bc48ca163ef65367b15751591c5bc
sourish19 <sourish19z@gmail.com>
2025-08-17 13:36:18 +0000

    Initialized project & completed the structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AppRoutes from './routes/AppRoutes'
+
+function App() {
+  return (
+    <div className='flex min-h-svh flex-col items-center justify-center'>
+      <AppRoutes />
+    </div>
+  )
+}
+
+export default App
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '@/pages/Home'
+import Signin from '@/pages/Signin'
+import Signup from '@/pages/Signup'
+import ForgotPassword from '@/pages/ForgotPassword'
+import ResetPassword from '@/pages/ResetPassword'
+import VerifyEmail from '@/pages/VerifyEmail'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='reset-password' element={<ResetPassword />} />
+      <Route path='/verify-email' element={<VerifyEmail />} />
+      <Route path='/' element={<Home />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
