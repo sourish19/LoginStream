e1bf5be641e76a7e219e9733eee606001fa7122c
sourish19 <sourish19z@gmail.com>
2025-09-06 08:30:24 +0000

    Initialized server & prisma

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express, { urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(urlencoded({extended: true}))
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log('Server running on PORT: ', PORT));
+
+export default app;
